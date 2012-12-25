c1041a9ecc68e00abc4e8f4eb8effbd720fb20a6
ray <ray@tomo.im>
2012-12-25 11:00:01 +0800

    优化代码，删除大部分js，利用遮罩完成动画

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 -function($) {
-  var bg = $('.bg-wrap'),
-      arr = ['first', 'second', 'third', 'forth', 'fifth', 'sixth', 'eighth', 'nighth', 'tenth'];
   $(document).ready(function() {
-    replaceClass('first');
-    bg[0].addEventListener('webkitTransitionEnd', function(e) {
-      var target = e.target;
-      triggerNext.call(target, 1, 2);
-    }, false);
+    $('.mask').addClass('mask-end').removeClass('mask');
   });
-  function triggerNext() {
-    var nodeName = this.className;
-    nodeName = nodeName.slice(0, -4);
-    var result = $.inArray(nodeName, arr);
-    if (result !== -1) {
-      var next = result + 1,
-          nextNode = arr[next];
-      replaceClass(nextNode);
-    }
-  }
-  function replaceClass(node) {
-    var name = '.' + node + '-start';
-    $(name).addClass(node + '-end').removeClass(node + '-start');
-  }
-
 }(window.jQuery);
