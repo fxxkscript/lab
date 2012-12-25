e33a1c0daa4cf3d23612acfa802ab207867edd7e
ray <ray@tomo.im>
2012-12-25 10:24:03 +0800

    h5bp

---
+++ js/main.js
@@ -0,0 +1,31 @@
+-function($) {
+  var bg = $('.bg-wrap'),
+      arr = ['first', 'second', 'third', 'forth', 'fifth', 'sixth', 'eighth', 'nighth', 'tenth'];
+  bg.mouseenter(function() {
+    replaceClass('first');
+  });
+  bg[0].addEventListener('webkitTransitionEnd', function(e) {
+    var target = e.target;
+    triggerNext.call(target, 1, 2);
+  }, false);
+
+  function triggerNext() {
+    var nodeName = this.className;
+    nodeName = nodeName.slice(0, -4);
+    var result = $.inArray(nodeName, arr);
+    if (result !== -1) {
+      var next = result + 1,
+          nextNode = arr[next];
+      replaceClass(nextNode);
+    }
+  }
+  function replaceClass(node) {
+    var name = '.' + node + '-start';
+    $(name).addClass(node + '-end').removeClass(node + '-start');
+  }
+
+}(window.jQuery);
+
+document.onload = function() {
+  console.log('hello world');
+};
